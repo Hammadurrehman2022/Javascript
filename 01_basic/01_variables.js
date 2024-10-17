00a62fc3dc658dc2dd2583f547bed3a266ca42a7
Hammad Ur Rahman <99906991+Hammadurrehman2022@users.noreply.github.com>
2024-10-17 15:45:23 +0000

    variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,15 @@
+const accountID = 324214;
+let accountEmail = "avcd@gmail.com";
+var accountPassword = "3424dsf@#4";
+accountCity = "Karachi";
+let accountState;
+
+// accountID = 243214; not allowed
+
+accountEmail = "afdas@gmail.com";
+accountPassword = "fafsdaa";
+accountCity = "Lahore";
+
+console.log(accountID,accountEmail);
+
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState])
