d038440d1ce04eeb7f3f0748e9fc889f5c0d0f43
Hammad Ur Rahman <99906991+Hammadurrehman2022@users.noreply.github.com>
2024-10-17 16:35:40 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict" // treat all js code as newer version
+
+let name = "Hammad"
+let age = 15
+let isLogIn = true
+let state
+
+// Primitive data types
+// number
+// bigint
+// string
+// boolean => ture/false
+// null => standalone value
+// symbol => unique
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
