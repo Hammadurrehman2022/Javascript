9ba7c5262d9bcac18fee9f7061cbc7101eaeaf19
Hammad Ur Rahman <99906991+Hammadurrehman2022@users.noreply.github.com>
2024-10-20 03:50:45 +0000

    conversioin on datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = true;
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(valueInNumber);
+
+// "19" => 19
+// "19abc" => Nan
+// ture => 1, false => 0
+
+let isLoggedIn = "yes";
+
+let booleanIsloggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsloggedIn);
+
+// 1 => true, 0 => false
+// "" => false
+// "text" => true
+
+let number = 23;
+let stringNumber = String(number);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
