beeff6b56721417376272e7664c9a98d2f06df20
Hammad Ur Rahman <99906991+Hammadurrehman2022@users.noreply.github.com>
2024-10-22 06:22:16 +0000

    Operations

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,3 +1,4 @@
+"use strict"
 let score = true;
 // console.log(typeof score);
 // console.log(typeof(score));
@@ -22,3 +23,46 @@ let number = 23;
 let stringNumber = String(number);
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
+
+// .............. Operations .............
+
+let value = 4;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log( 2+3 );
+// console.log( 3-5 );
+// console.log( 3*2 );
+// console.log( 2**3);
+// console.log( 4/2 );
+// console.log( 9%2 );
+
+let opPrec = (3+3) * 5 / 5;
+// console.log(opPrec); 
+
+// console.log( +true );
+// console.log( -true );
+// console.log( +"" );
+// console.log( +false );
+// console.log( +"text" );
+
+
+let greeting = "Hello";
+let name = " Hammad"
+let sentence = greeting + name;
+// console.log(sentence);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+// console.log( gameCounter );
+// console.log(++gameCounter);
+// console.log( gameCounter);
+
