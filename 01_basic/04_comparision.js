57ca533023d01e53d6f38550bb6be0ff5949c8cc
Hammad Ur Rahman <99906991+Hammadurrehman2022@users.noreply.github.com>
2024-10-22 06:59:57 +0000

    comparision operator

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,27 @@
+// console.log( 2>3 );
+// console.log( 2<4);
+// console.log( 2==4 );
+// console.log( 2<=5);
+// console.log( 7>=6 );
+// console.log( 3!=5 );
+
+
+// console.log( "3" == 3);
+// console.log( "3" > 2);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// Equality check == and comparision > < >= <= work deifferently
+// comparisions convert null to 0 that's why null >= 0 is true
+
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// ===
+console.log( "5" === 5 );
+
+// === also check datatypes
