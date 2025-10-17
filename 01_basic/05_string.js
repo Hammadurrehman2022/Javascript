28fc72d43f9bb443c12ad42aba24b6e54fcbe53f
Hammad Ur Rahman <99906991+Hammadurrehman2022@users.noreply.github.com>
2025-10-17 16:36:06 +0000

    more about string

---
+++ 01_basic/05_string.js
@@ -0,0 +1,11 @@
+    //concatenation
+const userName = "Hammad "
+const score = 12
+// console.log(userName + "score " + score);  // old way
+
+// string interpolation
+console.log(`Player ${userName.trim()} score ${score} `) // modern way
+
+// another way of string decleration
+const newUser = new String(1);
+console.log(newUser)
