074dfdf981450395d3d3a2efecc633db533e6f5c
Hammad Ur Rahman <99906991+Hammadurrehman2022@users.noreply.github.com>
2025-10-19 15:38:00 +0000

    Numbers and Maths

---
+++ 01_basic/06_numsAndmaths.js
@@ -0,0 +1,37 @@
+const quantity = 1432
+
+let balance = new Number(1234)
+// console.log(balance);
+
+// console.log(balance.toString())
+// console.log(quantity.toString())
+
+// console.log(balance.toFixed(2))
+
+const distance = 134.5764
+// console.log(distance.toPrecision(3))
+
+const budget = 200000000
+// console.log(budget.toLocaleString('ur-PK'))
+
+/* +++++++++++++ Maths +++++++++ */
+
+console.log(Math)
+// console.log(Math.abs(-2))
+// console.log(Math.round(7.54))
+// console.log(Math.ceil(2.3));
+// console.log(Math.floor(4.6));
+
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+let min = 100000
+let max = 999999
+
+console.log(Math.floor(Math.random() * (max - min )) + min); // simple otp generator
+
+
+
+
+
