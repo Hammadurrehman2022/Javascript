8132464b15caa416c6d18233ddbf02787f4bfc15
Hammad Ur Rahman <99906991+Hammadurrehman2022@users.noreply.github.com>
2025-10-22 14:45:55 +0000

    Array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,43 @@
+// array
+
+const arr = [0, 1, 2, 3, 4, 5,]
+
+const arr2 = new Array(1, 2, 3, 4)
+
+// console.log(arr2[0]);
+
+// Array Method 
+
+// arr.push(6) // Add new value to the end of and array
+// arr.pop() // Removes last element
+
+// arr.unshift(9) // insert new value at the start 
+// arr.shift() // removes first element
+
+// console.log(arr.includes(4));
+// console.log(arr.indexOf(9));
+
+const newArr = arr.join("|") 
+// console.log(newArr);
+// // console.log(newArr[6]);
+// console.log(arr);
+
+// slice and splice
+
+console.log("A ", arr);
+const myArr1 = arr.slice(1, 3) 
+console.log(myArr1);
+console.log("B ", arr);
+const myArr2 = arr.splice(1, 3)
+console.log(myArr2);
+console.log("C ", arr);
+
+
+
+
+
+
+
+
+
+
