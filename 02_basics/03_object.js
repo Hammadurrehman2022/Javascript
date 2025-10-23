d5c78561b2b58be3226f6cba4b2d6c44b926bbd2
Hammad Ur Rahman <99906991+Hammadurrehman2022@users.noreply.github.com>
2025-10-23 16:02:12 +0000

    object

---
+++ 02_basics/03_object.js
@@ -0,0 +1,41 @@
+// singleton
+
+// object literals
+
+const sym = Symbol("key1")
+
+const user = {
+    name: "abc", 
+    "full name": "abc xyz",
+    age: 20,
+    [sym]: "mykey1",
+    email: "abc@gmail.com",
+    location: "Paris",
+    isLoggedin: false,
+    lastloginDays: ["Tuesday", "Friday"],
+}
+
+// console.log(user.name);
+// console.log(user["email"])
+// console.log(user["full name"]);
+// console.log(user[sym]);
+
+user.name = "ere"
+
+// Object.freeze(user)
+
+user.name = "uoo"
+
+// console.log(user);
+
+user.greeting = function(){
+    console.log("Welcome user")
+}
+
+user.greetingTwo = function(){
+    console.log(`Hello user ${this.name} `);
+}
+user.greeting()
+user.greetingTwo()
+
+
