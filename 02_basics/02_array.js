268fe64f7dc458bf19f1dc704ccd45aa3c9d64f5
Hammad Ur Rahman <99906991+Hammadurrehman2022@users.noreply.github.com>
2025-10-23 15:12:51 +0000

    Array Methods

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -11,7 +11,22 @@ const AvailableLocation = [...allLocation, ...PakLocation]
 // console.log(AvailableLocation);
 
 const newArr = [1, 2, 3, [4, 5, 6], [7, [8, 9]]]
-console.log(newArr.flat(1));
+const flatArr = newArr.flat(Infinity);
+// console.log(flatArr);
+
+console.log(Array.isArray("abcd"));
+console.log(Array.from("abcd"));
+console.log(Array.from({name: "xyz"})); // return empty array
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+console.log(Array.of(score1, score2, score3));
+
+
+
+
+
+
 
 
 
