68db6f139ffa51a127640199b969f0e9bdb96812
Hammad Ur Rahman <99906991+Hammadurrehman2022@users.noreply.github.com>
2025-10-25 14:48:17 +0000

    hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,8 +3,45 @@ let a = 300
 // var c = 300
 if (true) {
     let a = 20
-    const b = 34
-    console.log("inner a", a)
+    const b = 40
+    // console.log("inner a", a)
 }
 
-console.log(a);
+// console.log(a);
+
+function one(){
+    const userName = "hammad"
+
+    function two(){
+        const skill = "programmer"
+        // console.log(userName);
+    }
+    two()
+    // console.log(skill);
+}
+
+// one()
+
+if (true){
+    const name = "hammad"
+    if(name === "hammad"){
+        const score = 10
+        // console.log(name + score);
+    }
+    // console.log(score);
+}
+
+// ====== interesting concept ======
+
+console.log(addOne(2));
+function addOne(num){
+    return num +1
+}
+
+console.log(addtwo(2));
+var addtwo = function(num){
+    return num +2
+}
+
+
+
