04c570844e8975fdaf9fcfec0474cee686f2cf37
Hammad Ur Rahman <99906991+Hammadurrehman2022@users.noreply.github.com>
2025-10-30 11:33:52 +0000

    while and do while loop

---
+++ 05_iterations/02_while.js
@@ -0,0 +1,20 @@
+let index = 0;
+while (index <= 10) {
+    console.log(index)
+    index = index + 1
+}
+
+let arr = ['Karachi', "Lahore", "Queta"]
+let i = 0;
+while (i < arr.length) {
+    console.log(arr[i])
+    i++
+}
+
+
+let score = 11
+let j =1;
+do {
+    console.log(score); 
+    console.log(j++)   
+} while (score <= 10 );
